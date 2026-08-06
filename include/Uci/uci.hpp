aaa89c0605bcf8991e105d69174c55d2aa5e65d2
SaltyyCode <clementbouret59@gmail.com>
2026-08-06 09:59:58 +0200

    feat(uci) added board inclusion in .hpp

--- include/Uci/uci.hpp
+++ include/Uci/uci.hpp
@@ -6,6 +6,7 @@
 #include <sstream>
 #include <unordered_map>
 #include <functional>
+#include "Board/board.hpp"
 
 class UciHandler {
 public:
@@ -15,6 +16,8 @@ public:
 private:
     std::unordered_map<std::string, std::function<void(std::istringstream&)>> commands_;
 
+    Board board_;
+    
     void handle_uci(std::istringstream& stream);
     void handle_isready(std::istringstream& stream);
     void handle_ucinewgame(std::istringstream& stream);
