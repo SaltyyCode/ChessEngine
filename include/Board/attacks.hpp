2531f7ae642d9999b2d4fb577e6116f4094cb235
SaltyyCode <clementbouret59@gmail.com>
2026-08-06 16:43:00 +0200

    feat(attacks): methods declaration for pieces attacks

--- include/Board/attacks.hpp
+++ include/Board/attacks.hpp
@@ -11,16 +11,28 @@ constexpr U64 NOT_B_FILE  = 0xFCFCFCFCFCFCFCFCULL; // Efface les colonnes A et B
 constexpr U64 NOT_G_FILE  = 0x3F3F3F3F3F3F3F3FULL; // Efface les colonnes G et H
 constexpr U64 NOT_H_FILE  = 0x7F7F7F7F7F7F7F7FULL; // Efface la colonne H
 
-
 extern U64 pawn_attacks[2][64];
 extern U64 knight_attacks[64];
 extern U64 king_attacks[64];
 
+extern U64 bishop_masks[64];
+extern U64 rook_masks[64];
+
+void init_leaper_attacks();
+void init_sliders_attacks(bool is_bishop);
+void init_all_attacks();
+
+U64 mask_pawn_attacks(int side, int sq);
+U64 mask_knight_attacks(int sq);
+U64 mask_king_attacks(int sq);
 U64 mask_bishop_attacks(int sq);
 U64 mask_rook_attacks(int sq);
 U64 bishop_attacks_on_the_fly(int sq, U64 block);
 U64 rook_attacks_on_the_fly(int sq, U64 block);
+U64 set_occupancy(int index, int bits_in_mask, U64 attack_mask);
 
-void init_leaper_attacks();
+U64 get_bishop_attacks(int sq, U64 occupancy);
+U64 get_rook_attacks(int sq, U64 occupancy);
+U64 get_queen_attacks(int sq, U64 occupancy);
 
 #endif
