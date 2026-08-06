b02e6c63ee5d011b8d0d5e6b2800e24af3132d2e
SaltyyCode <clementbouret59@gmail.com>
2026-08-06 14:14:10 +0200

    feat(attacks) hpp file with method declaration & mask for knights attacks squares

---
+++ include/Board/attacks.hpp
@@ -0,0 +1,26 @@
+#ifndef ATTACKS_HPP
+#define ATTACKS_HPP
+
+#include <cstdint>
+
+using U64 = std::uint64_t;
+
+// Masques de colonnes pour empêcher le chevauchement (wrap-around)
+constexpr U64 NOT_A_FILE  = 0xFEFEFEFEFEFEFEFEULL; // Efface la colonne A
+constexpr U64 NOT_B_FILE  = 0xFCFCFCFCFCFCFCFCULL; // Efface les colonnes A et B
+constexpr U64 NOT_G_FILE  = 0x3F3F3F3F3F3F3F3FULL; // Efface les colonnes G et H
+constexpr U64 NOT_H_FILE  = 0x7F7F7F7F7F7F7F7FULL; // Efface la colonne H
+
+
+extern U64 pawn_attacks[2][64];
+extern U64 knight_attacks[64];
+extern U64 king_attacks[64];
+
+U64 mask_bishop_attacks(int sq);
+U64 mask_rook_attacks(int sq);
+U64 bishop_attacks_on_the_fly(int sq, U64 block);
+U64 rook_attacks_on_the_fly(int sq, U64 block);
+
+void init_leaper_attacks();
+
+#endif
